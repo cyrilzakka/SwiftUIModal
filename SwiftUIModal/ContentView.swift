d747a892c4e2efc612c4747db350de1ab003409c
Cyril Zakka <cyril.zakka@gmail.com>
2019-07-31 11:50:58 +0300

    Initial Commit

---
+++ SwiftUIModal/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIModal
+//
+//  Created by Cyril Zakka on 7/31/19.
+//  Copyright © 2019 Cyril Zakka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
